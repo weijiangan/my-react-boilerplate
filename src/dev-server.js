9651a2333a7f5faf682559a4386c64473c7421b8
Wei Jian Gan <weijiangan@outlook.com>
2019-09-22 17:26:24 +0800

    update dev-server

--- src/dev-server.js
+++ src/dev-server.js
@@ -1,26 +1,31 @@
 const path = require("path");
 const express = require("express");
-const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackHotMiddleware = require("webpack-hot-middleware");
-const config = require("../config/webpack.dev");
 
 const app = express();
-const compiler = webpack(config);
 const PORT = process.env.PORT || 4000;
 
-app.use(
-  webpackDevMiddleware(compiler, {
-    logLevel: "warn",
-    publicPath: config.output.publicPath
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  const webpack = require("webpack");
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackHotMiddleware = require("webpack-hot-middleware");
+  const config = require("../config/webpack.dev");
+  const compiler = webpack(config);
 
-app.use(webpackHotMiddleware(compiler));
+  app.use(
+    webpackDevMiddleware(compiler, {
+      logLevel: "warn",
+      publicPath: config.output.publicPath
+    })
+  );
+
+  app.use(webpackHotMiddleware(compiler));
+}
 
 app.use("/", express.static(path.join(__dirname, "..", "dist")));
 
-app.get("*", (req, res, next) => {
+// for SPAs: route non-file routes (without extension) to index
+app.get("^[^\\.]+$", (req, res, next) => {
+  // for use with graphql-yoga
   if (req.url === "/graphql") return next();
   res.sendFile(
     "index.html",
